91320bd0365c8bca6dbac1a18354282dc3a6bd70
PrathibhaM1011 <prathibhamamindla2910@gmail.com>
2025-03-01 11:11:23 +0530

    Update 27-2.js

--- 28-2/27-2.js
+++ 28-2/27-2.js
@@ -22,7 +22,7 @@ function findLarge(num1,num2,callback){
           return num1+num2;
       }
       else if(num2 > num1){
-          return num1-num2;
+          return num2-num1;
       }
       else if(num1==num2){
           return 0;
